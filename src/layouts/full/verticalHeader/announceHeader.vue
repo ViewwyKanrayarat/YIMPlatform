3e036a17e74eb71b37550396450c0f969f1e55a3
Kanrayarat Srichai <kanrayarat.sc@gmail.com>
2025-11-11 11:21:10 +0700

    [UPDATE][2025-11-11 11:21] ดึงโปรโมชั่นจาก api มาแสดง

--- src/layouts/full/verticalHeader/announceHeader.vue
+++ src/layouts/full/verticalHeader/announceHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app-bar height="35" elevation="0" color="yellow">
     <div class="d-flex flex-column w-full h-full">
-      <div class="text-center py-1 ">For free shipping on order over 50,000 and more discount use code YIMPLATFORM</div>
+      <div class="text-center py-1 ">For free shipping on order over {{ formatNumber(announce.listItems?.config?.delivery_fee?.minimum_cart_amount ?? 0) }} and more discount use code {{ announce.listItems?.config?.promotion?.discount_code ?? '' }}</div>
     </div>
   </v-app-bar>
 </template>
@@ -10,8 +10,16 @@
 import { useAnnounce } from "@/stores/announce"
 const announce = useAnnounce()
 
-function init() {
-  announce.getAnnounce()
+// format number
+const formatNumber = (num: number) => {
+  return new Intl.NumberFormat().format(num)
+}
+
+async function init() {
+  
+  
+  await announce.getAnnounce()
+  console.log('List',announce.listItems.config.delivery_fee.minimum_cart_amount);
 }
 
 init()

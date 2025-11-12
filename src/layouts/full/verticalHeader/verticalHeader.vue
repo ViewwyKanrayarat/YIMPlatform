50241937b48dd56d6bfa0ee7d17de5d518a5a434
Kanrayarat Srichai <kanrayarat.sc@gmail.com>
2025-11-12 13:43:23 +0700

    [UPDATE][2025-11-12 13:34] เพิ่มโลโก้

--- src/layouts/full/verticalHeader/verticalHeader.vue
+++ src/layouts/full/verticalHeader/verticalHeader.vue
@@ -1,11 +1,13 @@
 <template>
   <v-app-bar height="70" elevation="0" color="orange">
     <div class="d-flex justify-space-between align-center px-10" style="flex: 1">
-      <div>Logo</div>
+      <!-- logo -->
+      <div>
+        <v-img src="/public/icon/smileIcon.png" height="60" width="60"/>
+      </div>
+      <!-- button cart -->
       <div v-if="props.cartProduct">
-        <!-- <v-chip color="red" size="small" text="81" variant="flat"></v-chip>
-        <v-btn class="text-none" size="large" color="#4f545c" prepend-icon="mdi-cart" variant="flat"> ฿0.00 </v-btn> -->
-        <v-btn height="50"  rounded="xl" class="button-cart px-4 py-2" elevation="2" @click="goToCart">
+        <v-btn height="50" rounded="xl" class="button-cart px-4 py-2" elevation="2" @click="goToCart">
           <div class="d-flex align-center">
             <v-badge :content="cartCount" color="red" floating>
               <v-icon color="black" size="20">mdi-cart</v-icon>
@@ -20,24 +22,26 @@
 
 <script setup lang="ts">
 import { ref } from "vue"
-import { useRouter } from "vue-router";
-const router = useRouter();
+import { useRouter } from "vue-router"
+const router = useRouter()
 
 const cartCount = ref(10)
+
 type propsModel = {
   cartProduct?: boolean
 }
+
 const props = withDefaults(defineProps<propsModel>(), {
   cartProduct: false,
 })
 
 const goToCart = () => {
-  router.push("/home/cart");
-};
+  router.push("/home/cart")
+}
 </script>
 
 <style scoped>
-.button-cart{
+.button-cart {
   background-color: #fff;
 }
 </style>

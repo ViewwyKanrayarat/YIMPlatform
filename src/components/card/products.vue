98db44d40f57120882196109d81bf90e1df433e9
Kanrayarat Srichai <kanrayarat.sc@gmail.com>
2025-11-13 16:54:39 +0700

    [UPDATE][2025-11-13 16:52] สร้างไฟล์ style

--- src/components/card/products.vue
+++ src/components/card/products.vue
@@ -12,14 +12,16 @@
     <!-- ราคา -->
     <div class="d-flex justify-space-between">
       <div>฿{{ props.items.price.toFixed(2) }} / EA</div>
-      <v-btn class="button-add" prepend-icon="mdi-plus" @click.stop="cart.getProductsInCart(props.items)"> Add to cart </v-btn>
+      <v-btn class="button-add" prepend-icon="mdi-plus" @click.stop="addProductToCart(props.items)"> Add to cart </v-btn>
     </div>
   </v-card>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue"
+import Swal from 'sweetalert2'
 import { useRouter } from "vue-router"
+import type { CartModel } from "@/types/CartType"
 import { useCart } from "@/stores/cart"
 const cart = useCart()
 const router = useRouter()
@@ -50,40 +52,19 @@ const props = withDefaults(defineProps<propsModel>(), {
 function goToDetail() {
   router.push(`home/detail/${props.items.sku}`)
 }
+
+function addProductToCart(items:CartModel) {
+  cart.getProductsInCart(items)
+  Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Add to cart success',
+    showConfirmButton: false,
+    timer: 1500
+  })
+}
 </script>
 
 <style scoped>
-.layout-card {
-  width: 100%;
-}
-
-.img-resize {
-  height: 250px;
-}
-.text-sku {
-  color: gray;
-  margin-top: 10px;
-}
-.text-name {
-  font-size: 18px;
-  margin-bottom: 20px;
-}
-.button-add{
-  color: white;
-  background-color: #F14725;
-
-}
-@media (max-width: 850px) {
-  .img-resize {
-    height: 150px;
-    width: 100%;
-  }
-}
-
-@media (max-width: 600px) {
-  .img-resize {
-    height: 250px;
-    width: 100%;
-  }
-}
+@import "@/css/components/products.css";
 </style>
